fce89ae2f60e5aef5bba85f8609cac14a6d5be98
Ryan Doyle <doylecodes@gmail.com>
2019-08-18 14:42:20 -0700

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const index = () => (
+  <Title>
+    This is a Title!
+  </Title>
+);
+
+const Title = styled.h1`
+  color: red;
+`;
+
+export default index;
